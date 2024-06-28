15aa7dc45e62bf089c1620362b9ffd1ca01754b2
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-27 19:07:58 -0700

    CS V11

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     Code Your Program here
     """
     for i in range(N + 1):
-        print (i ** 2)
+        print (2 ** i)
     
     """
     ########################################
